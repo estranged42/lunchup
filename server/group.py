2ebaf27bb2e8a46e310e370cf497618f2031e024
Mark Fischer <fischerm@email.arizona.edu>
2017-05-06 19:03:53 -0700

    Moved stats generation to be group specific

--- server/group.py
+++ server/group.py
@@ -3,6 +3,7 @@ A group class
 '''
 import logging
 import json
+from random import randrange
 
 class group:
   
@@ -10,31 +11,112 @@ class group:
     '''
     Init a new group object.
     '''
+    # List to hold the members of thi group (Person objects)
+    self.members = []
+    
+    # The ultimate similarity score of this group (Higher is more similar)
     self.group_score = 0
     self.group_index = 0
-    self.members = []
+    self.score_average = 0
+    
+    # Attribute Counts data structure for the group
+    self.att_counts = {}
+    self.att_freq = {}
+
+    # Total Sum of all Frequency
+    self.att_freq_total = 0
+    
     logging.debug("New Group: {:s}".format( repr(self) ))
 
   def addPerson(self, p):
     self.members.append(p)
   
   def score_group(self):
+    self.compute_stats()
+    
     for p in self.members:
       self.group_score += p.get_similarity_score()
       self.group_index += p.get_similarity_index()
+      self.score_average = self.group_score / len(self.members)
 
   def get_group_score(self):
     return self.group_score
 
+
+  def get_attr_counts(self):
+    return self.att_counts
+  
+
+  def compute_stats(self):
+    # Reset Everything
+    self.group_score = 0
+    self.group_index = 0
+    self.score_average = 0
+    self.att_counts = {}
+    self.att_freq = {}
+    self.att_freq_total = 0
+
+    num_people = len(self.members)
+
+    # First we need to count up how many attributes are held by what people
+    for p in self.members:
+      atts = p.get_attributes()
+      # The following creates a multi-demential map, with attributes at the first
+      # level, and values for the next level.  The ultimate value for a [att][val] 
+      # index is the count of times that value was used for that attribute.
+      for k,v in atts.items():
+        if k not in self.att_counts:
+          self.att_counts[k] = {}
+        if v not in self.att_counts[k]:
+          self.att_counts[k][v] = 0
+        self.att_counts[k][v] += 1
+
+    
+    # Next we need to get the sum of the frequency of each attribute
+    # In the same loop, sum up the frequencies for each person
+    for p in self.members:
+      sim_score = 0
+      attrs = p.get_attributes()
+      for attr in attrs:
+        p_freq = self.att_counts[attr][ attrs[attr] ]
+        sim_score += p_freq
+        
+        if attr not in self.att_freq:
+          self.att_freq[attr] = {}
+          self.att_freq[attr]['sum'] = 0
+        
+        self.att_freq[attr]['sum'] += p_freq
+        self.att_freq_total += p_freq
+      p.sim_score = sim_score
+    
+    # Next compute the avg frequency for each attribute
+    for attr,stats in self.att_freq.items():
+      self.att_freq[attr]['avg'] = self.att_freq[attr]['sum'] / num_people
+      logging.debug("{:12s}  Sum:{:5n}  Avg:{:5n}".format(attr, stats['sum'], stats['avg']) )
+    
+    logging.debug("att_freq_total: {:5n}".format(self.att_freq_total) )
+  
+    for p in self.members:
+      p.sim_index = p.sim_score / self.att_freq_total * 100
+      logging.debug("{:25s}  Sim Score: {:5n}    Sim Inex: {:5n}".format(p.name, p.sim_score, p.sim_index) )
+
+
+  def remove_random_person(self):
+    '''
+    Pick a random person from our group and remove them.  Return the removed person
+    '''
+    p_index = randrange( len(self.members) )
+    return self.members.pop(p_index)
+
   def __repr__(self):
     '''
-    Print out a basic representation of a person object
+    Print out a basic representation of a group object
     '''
     names = []
     for m in self.members:
       names.append( m.name )
     names_string = ", ".join(names)
-    return_string = "[ {:s} ] Score: {:n}   Index: {:n}".format(names_string, self.group_score, self.group_index)
+    return_string = "[ {:s} ] Score: {:n}   Index: {:n}".format(names_string, self.group_score, self.score_average)
       
     return return_string
 
--- server/person.py
+++ server/person.py
@@ -5,16 +5,7 @@ import logging
 import json
 
 class person:
-
-  # Counts of each attribute
-  att_counts = {}
-  
-  # Sum of each attribute frequency
-  att_freq = {}
-  
-  # Total Sum of all Frequency
-  att_freq_total = 0
-  
+    
   def __init__(self, init_data):
     '''
     Init a new person object.
@@ -27,14 +18,6 @@ class person:
     for k,v in init_data.items():
       # print( "{:s}, {:s}".format(k,v) )
       self.att[k] = v
-      
-      if k not in self.att_counts:
-        self.att_counts[k] = {}
-      
-      if v not in self.att_counts[k]:
-        self.att_counts[k][v] = 0
-      
-      self.att_counts[k][v] += 1
     
     logging.debug("New Person: {:s}".format( repr(self) ))
 
@@ -43,44 +26,13 @@ class person:
 
   def get_similarity_index(self):
     return self.sim_index
+  
+  def get_attributes(self):
+    return self.att
 
   @property
   def name(self):
     return self.att['name']
-
-  @staticmethod
-  def get_attr_counts():
-    return person.att_counts
-  
-  @staticmethod
-  def compute_stats(person_list):
-    num_people = len(person_list)
-    # First we need to get the sum of the frequency of each attribute
-    # In the same loop, sum up the frequencies for each person
-    for p in person_list:
-      sim_score = 0
-      for attr in p.att:
-        p_freq = person.att_counts[attr][ p.att[attr] ]
-        sim_score += p_freq
-        
-        if attr not in person.att_freq:
-          person.att_freq[attr] = {}
-          person.att_freq[attr]['sum'] = 0
-        
-        person.att_freq[attr]['sum'] += p_freq
-        person.att_freq_total += p_freq
-      p.sim_score = sim_score
-    
-    # Next compute the avg frequency for each attribute
-    for attr,stats in person.att_freq.items():
-      person.att_freq[attr]['avg'] = person.att_freq[attr]['sum'] / num_people
-      logging.debug("{:12s}  Sum:{:5n}  Avg:{:5n}".format(attr, stats['sum'], stats['avg']) )
-    
-    logging.debug("att_freq_total: {:5n}".format(person.att_freq_total) )
-  
-    for p in person_list:
-      p.sim_index = p.sim_score / person.att_freq_total * 100
-      logging.debug("{:25s}  Sim Score: {:5n}    Sim Inex: {:5n}".format(p.name, p.sim_score, p.sim_index) )
 
   def __repr__(self):
     '''
